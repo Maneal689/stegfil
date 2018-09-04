80a8f2c86d76d23b4bbe17d7a3f663d2461ef6a6
Maneal689 <g3douilly@gmail.com>
2018-09-04 11:46:31 +0200

    Adding binary fusion fonction and binary display fonction with main tests

---
+++ CMakeFiles/stegfil.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stegfil.dir/src/bin_op.cpp.o"
+  "bin/stegfil.pdb"
+  "bin/stegfil"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stegfil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
